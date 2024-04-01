f1590c479a5980f9da40aadd1526363a8092d83c
Ahsan Abbas <ahsanabbas468@gmail.com>
2024-04-01 15:22:18 +0300

    part3 exercise 3.8 commit
    Exercise 3.8 completion commit

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('req-data', (req,res)=>{
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-data'))
 
 persons = [
     { 
@@ -84,10 +89,10 @@ app.post('/api/persons', (req,res)=> {
 
     const id = Math.floor(Math.random()* 10000);
 
-    person.id = id
-    persons = persons.concat(person)
+    const newPerson = {...person, 'id':id}
+    persons = persons.concat(newPerson)
 
-    res.json(person)
+    res.json(newPerson)
 })
 
 // Start server
